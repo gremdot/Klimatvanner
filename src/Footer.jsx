d75757d039222ce41c8f950c07f435b1545725f6
Naomi Ruokamo <naomiruokamo@gmail.com>
2024-12-13 10:45:29 +0100

    Added Footer desc. & changed div className

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,23 +4,25 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer>
-      <div className="footer">
+      <div className="footer"> 
         <section>
-        <div className="container">
+        <div className="footer-container">
           <div className="footer-logo">
+            <a href="/">
             <img src="./klimatvanner_svart.png" alt="logo"/>
+            </a>
           </div>
           </div>
           <div className="footer-links">
-            <div className="container">
-            <h4>Klimatet</h4>
+            <div className="footer-container">
+            <h4>Klimatet</h4> 
             <ul>
               <li><a href="/Klimatnyheter">Klimatnyheter</a></li>
               <li><a href="/Fakta-om">Fakta om koldioxidutsläpp</a></li>
               <li><a href="/Lär-dig">Lär dig om klimatet</a></li>
               </ul>
             </div>
-            <div className="container">
+            <div className="footer-container">
             <h4>Organisation</h4>
             <ul>
               <li><a href="/Kontakta-oss">Kontakta Oss</a></li>
@@ -30,6 +32,7 @@ const Footer = () => {
           </div>
         </section>
       </div>
+      {/*Footer består av tre div med samma klass "footer-container". Den första består av en <img> av klimatvänners logo inuti <a> som leder till startsidan. De sista två har en egen <h4> rubrik och en <ul> (unordered list) med flera <li> (list item). Var <li> har en <a> (anchor element) som länkar till olika delar av webbplatsen. Denna Footer komponent används i App.jsx och inte individuellt på sidorna så den är alltid detsamma och finns på varje sida. */}
     </footer>
   );
 };
